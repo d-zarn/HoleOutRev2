78ee58e39090760775e57534067df0aa9a24c185
Dylan Zarn <zarndy77@gmail.com>
2025-05-23 12:22:48 -0500

    Added ScorecardView

--- HoleOutRev2/Views/PlayGolfView/ScorecardView/ScorecardView.swift
+++ HoleOutRev2/Views/PlayGolfView/ScorecardView/ScorecardView.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct ScorecardView: View {
+    
+    private let course: CourseModel
+    @EnvironmentObject private var courseService: CourseService
+    
+    init(for course: CourseModel) {
+        self.course = course
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            GroupBox{
+                HStack {
+                    
+                    StatItem("Front", "\(courseService.getFrontPar(for: course))")
+                    Spacer()
+                    StatItem("Back", "\(courseService.getBackPar(for: course))")
+                    Spacer()
+                    StatItem("Total", "\(course.par)")
+                    
+                }
+            }
+            .padding(.horizontal)
+            
+            VStack {
+                ForEach(Array(course.holes.sorted(by: { $0.holeNumber < $1.holeNumber }))) { hole in
+                    HoleCardView(hole: hole)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    ScorecardView()
+    let courseService = CourseService()
+    ScorecardView(for: courseService.getDefaultCourse())
+        .environmentObject(courseService)
 }
