37a03ae3f03bc64da75e12386cb563c598eaeea1
Dylan Zarn <zarndy77@gmail.com>
2025-05-26 23:08:31 -0500

    Small Round Card for details at a glance

--- HoleOutRev2/Views/HistoryView/RoundCardView.swift
+++ HoleOutRev2/Views/HistoryView/RoundCardView.swift
@@ -8,11 +8,50 @@
 import SwiftUI
 
 struct RoundCardView: View {
+    
+    private var round: RoundModel
+    @EnvironmentObject private var courseService: CourseService
+    
+    init(round: RoundModel) {
+        self.round = round
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GroupBox {
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Label("\(courseService.getCourse(byID: round.courseId)?.name ?? "Course Unavailable")", systemImage: "house.and.flag.fill")
+                            .fontWeight(.bold)
+                            .font(.headline)
+                            .foregroundStyle(.blue)
+                        Label("\(round.formattedDate)", systemImage: "calendar")
+                            .foregroundStyle(.secondary)
+                        Label("\(round.formattedDuration)", systemImage: "clock")
+                            .foregroundStyle(.secondary)
+                    }
+                    Spacer()
+                    RelativeScore(par: courseService.getTotalPar(by: round.courseId), score: round.totalScore, large: true)
+                }
+                Divider()
+                HStack {
+                    StatItem("Par", "\(courseService.getTotalPar(by: round.courseId))")
+                    Spacer()
+                    StatItem("Front", "\(round.frontScore)")
+                    Spacer()
+                    StatItem("Back", "\(round.backScore)")
+                    Spacer()
+                    StatItem("Final Score", "\(round.totalScore)")
+                }
+            }
+
+        }
+        .padding(.horizontal)
     }
 }
 
 #Preview {
-    RoundCardView()
+    let courseService = CourseService()
+    RoundCardView(round: RoundModelMock.completeRound())
+        .environmentObject(courseService)
 }
